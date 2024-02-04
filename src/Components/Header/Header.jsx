cdd7c766ed67c459de4fbb99d23bfb7c5691ef13
ak220193 <mail2meak22frcrio@gmail.com>
2024-02-04 12:05:38 +0530

    header component changes needed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Header.css";
+import CV from "../../Components/Header/CV";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Header>
+      <div className="container header__container">
+        <h5> Hi I'am </h5>
+        <h1> AKASH S M </h1>
+        <h5 className="text-light"> Full Stack developer </h5>
+
+        <CV />
+      </div>
+    </Header>
+  );
 };
 
 export default Header;
